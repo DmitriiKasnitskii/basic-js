2e98d0469e5b0d46f245ed3e52160406e3b15bb7
Dmitrii Kasnitskii <buildog@mail.ru>
2023-12-16 17:20:54 +0300

    fix: add final task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,69 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+function isUpperCase (character) {
+  if (character === character.toUpperCase()) {
+    return true
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  if (character === character.toLowerCase()) {
+    return false
+  }
+}
+
+function isLetter (str) {
+  return str.length === 1 && str.match(/[a-zA-Z]/i)
+}
+
+class VigenereCipheringMachine {
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+  }
+
+  encrypt(message, key) {
+    if (typeof message === 'undefined' || typeof key === 'undefined') {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let result = ''
+
+    for (let i = 0, j = 0; i < message.length; i++) {
+      const c = message.charAt(i)
+      if (isLetter(c)) {
+        if (isUpperCase(c)) {
+          result += String.fromCharCode((c.charCodeAt(0) + key.toUpperCase().charCodeAt(j) - 2 * 65) % 26 + 65) // A: 65
+        } else {
+          result += String.fromCharCode((c.charCodeAt(0) + key.toLowerCase().charCodeAt(j) - 2 * 97) % 26 + 97) // a: 97
+        }
+        j = ++j % key.length
+      } else {
+        result += c
+      }
+    }
+
+    return !this.isDirect ? result.toUpperCase().split('').reverse().join('') : result.toUpperCase();
+  }
+  decrypt(message, key) {
+    if (typeof message === 'undefined' || typeof key === 'undefined') {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let result = ''
+
+    for (let i = 0, j = 0; i < message.length; i++) {
+      const c = message.charAt(i)
+      if (isLetter(c)) {
+        if (isUpperCase(c)) {
+          result += String.fromCharCode(90 - (25 - (c.charCodeAt(0) - key.toUpperCase().charCodeAt(j))) % 26)
+        } else {
+          result += String.fromCharCode(122 - (25 - (c.charCodeAt(0) - key.toLowerCase().charCodeAt(j))) % 26)
+        }
+        j = ++j % key.length
+      } else {
+        result += c
+      }
+    }
+
+    return !this.isDirect ? result.toUpperCase().split('').reverse().join('') : result.toUpperCase();
   }
 }
 
